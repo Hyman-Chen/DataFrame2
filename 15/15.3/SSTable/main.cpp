bfc3519bb11bda65a69389d5f97ae652694e5801
Hyman-Chen <hymannnn@163.com>
2025-03-02 06:33:47 +0800

    binary-search

--- 15/15.3/SSTable/main.cpp
+++ 15/15.3/SSTable/main.cpp
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+// 定义顺序查找表
 typedef int ElemType;
 typedef struct {
     ElemType *elem;
     int TabelLen;
 } SSTable;
 
+// 初始化顺序表
 void ST_Init(SSTable &ST,int len){
     ST.TabelLen = len + 1;
     ST.elem = (ElemType *)malloc(sizeof(ElemType) * ST.TabelLen);
@@ -17,6 +20,7 @@ void ST_Init(SSTable &ST,int len){
     }
 }
 
+// 打印顺序表
 void ST_Print(SSTable ST){
     for (int i = 1; i < ST.TabelLen; i++) {
         printf("%3d", ST.elem[i]);
@@ -24,16 +28,17 @@ void ST_Print(SSTable ST){
     printf("\n");
 }
 
+// 顺序表查找
 int ST_Search(SSTable ST, ElemType key) {
     ST.elem[0] = key;
     int i;
     for (i = ST.TabelLen - 1; ST.elem[i] != key; i--) ;
     return i;
 }
+
 int main() {
     SSTable ST;
     ST_Init(ST, 10);
-
     ST_Print(ST);
     ElemType pos;
     printf("enter the number you want to search:\n");
